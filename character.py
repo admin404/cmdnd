ad67a6486bfb2aacd0c108b866a76c362171f9d3
ScoutV_workPC <neterror404@gmail.com>
2023-12-29 09:57:11 +0200

    2912230957

---
+++ character.py
@@ -0,0 +1,10 @@
+class Create:
+    """initiate the Character"""
+
+    def __init__(self, c_name, c_race, c_class, c_background, c_abilities):
+        self.C_Name = c_name
+        self.C_Race = c_race
+        self.C_Class = c_class
+        self.C_Background = c_background
+        self.C_Abilities = c_abilities
+        
